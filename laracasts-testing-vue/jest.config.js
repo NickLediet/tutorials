98e9cd05e52350adced7d8708588ce0852dda87c
Nicholas Lediet <nicholas.lediet@ihsmarkit.com>
2022-10-12 10:47:16 -0400

    Setup deps

---
+++ laracasts-testing-vue/jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  "jest": {
+    "moduleFileExtensions": ["js", "json", "vue"],
+    "transform": {
+      "^.+\\.vue$": "@vue/vue2-jest"
+    }
+  }
+};
+
+module.exports = config;
