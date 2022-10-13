30e4c757f1c428d026633c7a9a68e5afc71a8ed8
Nicholas Lediet <nicholas.lediet@ihsmarkit.com>
2022-10-13 10:38:12 -0400

    Add more tests

--- laracasts-testing-vue/src/components/Counter.vue
+++ laracasts-testing-vue/src/components/Counter.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>Current count is <span class="counter">{{count}}</span></h1>
+    <div class="counter">
+        <h1 class="counter__text">
+            Current count is: 
+            <span class="counter__count">{{count}}</span>
+        </h1>
+        <button 
+            @click="increment"
+            class="counter__add">+</button>
+        <button 
+            @click="decrement"
+            class="counter__decrement">-</button>
+
+    </div>
 </template>
 <script>
 export default {
@@ -7,6 +19,15 @@ export default {
     data() {
         return {
             count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count = (this.count > 0) ? 
+                (this.count - 1) : 0;
         }
     }
 }
--- laracasts-testing-vue/tests/counter.spec.js
+++ laracasts-testing-vue/tests/counter.spec.js
@@ -3,25 +3,59 @@ import { shallowMount } from '@vue/test-utils';
 import Counter from '../src/components/Counter.vue';
 
 
-describe('Counter.vue', () => {
-    it('should render', () => {
-        const wrapper = shallowMount(Counter);
 
+describe('Counter.vue', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Counter);
+    });
+
+    it('should render', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
     it('defaults to a count of zero', () => {
-        const wrapper = shallowMount(Counter);
         const count = wrapper.vm.count;
 
         expect(count).toBe(0);
     })
 
     it('presents the current count', () => {
-        const wrapper = shallowMount(Counter);
         const count = wrapper.vm.count;
-        const countEl = wrapper.find('.counter')
+        const countEl = wrapper.find('.counter__count')
 
         expect(parseInt(countEl.text())).toBe(count);
     });
+
+    it('should increment the value when increased', () => {
+       const addButtonEl = wrapper.find('.counter__add');
+
+       addButtonEl.trigger('click');
+       addButtonEl.trigger('click');
+      
+       // The count value should have incremented twice
+       expect(wrapper.vm.count).toBe(2);
+    });
+
+    it('should decrease the value when decremented', () =>  {
+        const decrementButtonEl = wrapper.find('.counter__decrement');
+        const addButtonEl = wrapper.find('.counter__add');
+        // Bring the count up to 2
+        addButtonEl.trigger('click');
+        addButtonEl.trigger('click');
+        // Decrement it to 1
+        decrementButtonEl.trigger('click');
+
+        expect(wrapper.vm.count).toBe(1);
+    });
+
+    it('should not decrease past zero', () => {
+        const decrementButtonEl = wrapper.find('.counter__decrement');
+
+        decrementButtonEl.trigger('click');
+        decrementButtonEl.trigger('click');
+        decrementButtonEl.trigger('click');
+
+        expect(wrapper.vm.count).toBe(0);
+    })
 });
